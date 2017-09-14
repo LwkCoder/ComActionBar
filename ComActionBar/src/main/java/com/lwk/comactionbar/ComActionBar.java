e62f7d0cf62b025fed04d17d3c8b4d9df5184ce3
luowenkang <505515031@qq.com>
2017-09-14 16:54:35 +0800

    release v1.1.0
    Change-Id: I7cd1163e6c829275fba42e240e04eb5d05f720a1

--- ComActionBar/src/main/java/com/lwk/comactionbar/ComActionBar.java
+++ ComActionBar/src/main/java/com/lwk/comactionbar/ComActionBar.java
@@ -8,6 +8,7 @@ import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.text.TextUtils;
 import android.util.AttributeSet;
+import android.util.TypedValue;
 import android.view.View;
 import android.view.ViewStub;
 import android.widget.FrameLayout;
@@ -23,6 +24,7 @@ import com.lwkandroid.widget.R;
  */
 public class ComActionBar extends FrameLayout
 {
+    private static final String TAG = "ComActionBar";
     protected View mViewLeft;
     protected TextView mTvLeft;
     protected ImageView mImgLeft;
@@ -33,6 +35,13 @@ public class ComActionBar extends FrameLayout
     protected TextView mTvRight02;
     protected ImageView mImgRight01;
     protected ImageView mImgRight02;
+
+    private boolean isLeftAsBack = false;
+    boolean isLeftBackWithoutText = false;
+    private int allTextColor = Color.WHITE;
+    private int backDrawableResId = R.drawable.icon_arrow_left;
+    private float titleTextSize;
+    private float itemTextSize;
 
     public ComActionBar(Context context)
     {
@@ -60,7 +69,6 @@ public class ComActionBar extends FrameLayout
     //初始化自定义属性
     private void initParams(Context context, AttributeSet attrs)
     {
-        boolean isLeftAsBack = false;
         String leftText = null;
         int leftImageResId = 0;
         String title = null;
@@ -72,7 +80,9 @@ public class ComActionBar extends FrameLayout
         int leftTextColor = Color.WHITE;
         int rightTextColor01 = Color.WHITE;
         int rightTextColor02 = Color.WHITE;
-        int bgColor = context.getApplicationContext().getResources().getColor(R.color.colorPrimary);
+        int bgColor = getResources().getColor(R.color.colorPrimary);
+        titleTextSize = getResources().getDimensionPixelSize(R.dimen.cab_title_size);
+        itemTextSize = getResources().getDimensionPixelSize(R.dimen.cab_text_size);
 
         TypedArray ta = context.obtainStyledAttributes(attrs, R.styleable.ComActionBar);
         if (ta != null)
@@ -84,47 +94,60 @@ public class ComActionBar extends FrameLayout
                 if (index == R.styleable.ComActionBar_bg_color)
                     bgColor = ta.getColor(index, bgColor);
                 else if (index == R.styleable.ComActionBar_is_left_as_back)
-                    isLeftAsBack = ta.getBoolean(index, false);
+                    isLeftAsBack = ta.getBoolean(index, isLeftAsBack);
                 else if (index == R.styleable.ComActionBar_left_text)
                     leftText = ta.getString(index);
                 else if (index == R.styleable.ComActionBar_left_image)
-                    leftImageResId = ta.getResourceId(index, 0);
+                    leftImageResId = ta.getResourceId(index, leftImageResId);
                 else if (index == R.styleable.ComActionBar_title_text)
                     title = ta.getString(index);
                 else if (index == R.styleable.ComActionBar_right_text01)
                     rightText01 = ta.getString(index);
                 else if (index == R.styleable.ComActionBar_right_image01)
-                    rightImageResId01 = ta.getResourceId(index, 0);
+                    rightImageResId01 = ta.getResourceId(index, rightImageResId01);
                 else if (index == R.styleable.ComActionBar_right_text02)
                     rightText02 = ta.getString(index);
                 else if (index == R.styleable.ComActionBar_right_image02)
-                    rightImageResId02 = ta.getResourceId(index, 0);
+                    rightImageResId02 = ta.getResourceId(index, rightImageResId02);
                 else if (index == R.styleable.ComActionBar_title_text_color)
-                    titleTextColor = ta.getColor(index, Color.WHITE);
+                    titleTextColor = ta.getColor(index, titleTextColor);
                 else if (index == R.styleable.ComActionBar_left_text_color)
-                    leftTextColor = ta.getColor(index, Color.WHITE);
+                    leftTextColor = ta.getColor(index, leftTextColor);
                 else if (index == R.styleable.ComActionBar_right_text_color01)
-                    rightTextColor01 = ta.getColor(index, Color.WHITE);
+                    rightTextColor01 = ta.getColor(index, rightTextColor01);
                 else if (index == R.styleable.ComActionBar_right_text_color02)
-                    rightTextColor02 = ta.getColor(index, Color.WHITE);
+                    rightTextColor02 = ta.getColor(index, rightTextColor02);
+                else if (index == R.styleable.ComActionBar_all_text_color)
+                    allTextColor = ta.getColor(index, allTextColor);
+                else if (index == R.styleable.ComActionBar_is_left_as_back_without_text)
+                    isLeftBackWithoutText = ta.getBoolean(index, isLeftBackWithoutText);
+                else if (index == R.styleable.ComActionBar_left_back_drawable)
+                    backDrawableResId = ta.getResourceId(index, backDrawableResId);
+                else if (index == R.styleable.ComActionBar_text_size_title)
+                    titleTextSize = ta.getDimension(index, titleTextSize);
+                else if (index == R.styleable.ComActionBar_text_size_item)
+                    itemTextSize = ta.getDimension(index, itemTextSize);
             }
             ta.recycle();
         }
 
         setBackgroundColor(bgColor);
 
+        if (isLeftBackWithoutText)
+            isLeftAsBack = true;
         if (isLeftAsBack)
         {
-            setLeftAsBack();
-            setLeftTextColor(leftTextColor);
-        } else if (!TextUtils.isEmpty(leftText))
+            refreshLeftBackStatus();
+        } else
         {
-            setLeftText(leftText);
-            setLeftTextColor(leftTextColor);
+            if (!TextUtils.isEmpty(leftText))
+            {
+                setLeftText(leftText);
+                setLeftTextColor(leftTextColor);
+            }
+            if (leftImageResId != 0)
+                setLeftImage(leftImageResId);
         }
-
-        if (leftImageResId != 0)
-            setLeftImage(leftImageResId);
 
         if (!TextUtils.isEmpty(title))
         {
@@ -151,74 +174,101 @@ public class ComActionBar extends FrameLayout
             setRightImage02(rightImageResId02);
     }
 
-    //inflate TextView in the left area
-    private void inflateLeftTextView()
+    protected void inflateLeftTextView()
     {
         if (mTvLeft == null)
         {
             ((ViewStub) findViewById(R.id.vs_comactionbar_left_text)).inflate();
             mTvLeft = (TextView) findViewById(R.id.tv_comactionbar_left);
+            mTvLeft.setTextColor(allTextColor);
+            mTvLeft.setTextSize(TypedValue.COMPLEX_UNIT_PX, itemTextSize);
         }
     }
 
-    /**
-     * set function:click left area as back/finish
-     */
     public void setLeftAsBack()
     {
+        setLeftAsBack(true);
+    }
+
+    public void setLeftAsBack(boolean b)
+    {
+        this.isLeftAsBack = b;
+        refreshLeftBackStatus();
+    }
+
+    public void setLeftAsBackWithoutText()
+    {
+        setLeftAsBackWithoutText(true);
+    }
+
+    public void setLeftAsBackWithoutText(boolean b)
+    {
+        this.isLeftBackWithoutText = b;
+        if (isLeftBackWithoutText)
+            this.isLeftAsBack = true;
+        refreshLeftBackStatus();
+    }
+
+    public void setLeftBackDrawable(int resId)
+    {
+        this.backDrawableResId = resId;
+        refreshLeftBackStatus();
+    }
+
+    protected void refreshLeftBackStatus()
+    {
         inflateLeftTextView();
-        Drawable drawable = getResources().getDrawable(R.drawable.icon_arrow_left);
-        int width = getResources().getDimensionPixelSize(R.dimen.cab_back_drawable_width);
-        int height = getResources().getDimensionPixelSize(R.dimen.cab_back_drawable_height);
-        drawable.setBounds(0, 0, width, height);
-        mTvLeft.setCompoundDrawables(drawable, null, null, null);
-        mTvLeft.setCompoundDrawablePadding(getResources().getDimensionPixelOffset(R.dimen.cab_back_drawable_padding));
-        if (TextUtils.isEmpty(mTvLeft.getText()))//set default text
-            mTvLeft.setText(R.string.cab_back);
-        if (getContext() instanceof Activity)
+        if (isLeftAsBack)
         {
-            setLeftClickListener(new OnClickListener()
+            Drawable drawable = getResources().getDrawable(backDrawableResId);
+            int width = getResources().getDimensionPixelSize(R.dimen.cab_back_drawable_width);
+            int height = getResources().getDimensionPixelSize(R.dimen.cab_back_drawable_height);
+            drawable.setBounds(0, 0, width, height);
+            mTvLeft.setCompoundDrawables(drawable, null, null, null);
+            mTvLeft.setCompoundDrawablePadding(getResources().getDimensionPixelOffset(R.dimen.cab_back_drawable_padding));
+            if (!isLeftBackWithoutText)
+                mTvLeft.setText(R.string.cab_back);
+            else
+                mTvLeft.setText(" ");
+
+            if (getContext() instanceof Activity)
             {
-                @Override
-                public void onClick(View v)
+                setLeftClickListener(new OnClickListener()
                 {
-                    ((Activity) getContext()).finish();
-                }
-            });
+                    @Override
+                    public void onClick(View v)
+                    {
+                        ((Activity) getContext()).finish();
+                    }
+                });
+            }
+        } else
+        {
+            mTvLeft.setText(null);
+            mTvLeft.setCompoundDrawables(null, null, null, null);
         }
     }
 
-    /**
-     * set text of the TextView in left area
-     */
+    public void setLeftTextColor(int color)
+    {
+        inflateLeftTextView();
+        if (color > 0)
+            mTvLeft.setTextColor(color);
+    }
+
     public void setLeftText(String text)
     {
         inflateLeftTextView();
         mTvLeft.setText(text);
     }
 
-    /**
-     * set text resource id of the TextView in left area
-     */
     public void setLeftText(int resId)
     {
         inflateLeftTextView();
         mTvLeft.setText(resId);
     }
 
-    /**
-     * set text color of the TextView in left area
-     *
-     * @param color
-     */
-    public void setLeftTextColor(int color)
-    {
-        inflateLeftTextView();
-        mTvLeft.setTextColor(color);
-    }
-
-    //inflate ImageView in the left area
-    private void inflateLeftImage()
+    protected void inflateLeftImage()
     {
         if (mImgLeft == null)
         {
@@ -272,12 +322,14 @@ public class ComActionBar extends FrameLayout
     }
 
     //inflate the TextView of title
-    private void inflateTitleTextView()
+    protected void inflateTitleTextView()
     {
         if (mTvTitle == null)
         {
             ((ViewStub) findViewById(R.id.vs_comactionbar_title)).inflate();
             mTvTitle = (TextView) findViewById(R.id.tv_comactionbar_title);
+            mTvTitle.setTextColor(allTextColor);
+            mTvTitle.setTextSize(TypedValue.COMPLEX_UNIT_PX, titleTextSize);
         }
     }
 
@@ -307,7 +359,8 @@ public class ComActionBar extends FrameLayout
     public void setTitleTextColor(int color)
     {
         inflateTitleTextView();
-        mTvTitle.setTextColor(color);
+        if (color > 0)
+            mTvTitle.setTextColor(color);
     }
 
     //inflate the first TextView in the right area
@@ -317,6 +370,8 @@ public class ComActionBar extends FrameLayout
         {
             ((ViewStub) findViewById(R.id.vs_comactionbar_right_text01)).inflate();
             mTvRight01 = (TextView) findViewById(R.id.tv_comactionbar_right01);
+            mTvRight01.setTextColor(allTextColor);
+            mTvRight01.setTextSize(TypedValue.COMPLEX_UNIT_PX, itemTextSize);
         }
     }
 
@@ -346,7 +401,8 @@ public class ComActionBar extends FrameLayout
     public void setRightTextColor01(int color)
     {
         inflateRightTextView01();
-        mTvRight01.setTextColor(color);
+        if (color > 0)
+            mTvRight01.setTextColor(color);
     }
 
     //infalte the second TextView in right area
@@ -356,6 +412,8 @@ public class ComActionBar extends FrameLayout
         {
             ((ViewStub) findViewById(R.id.vs_comactionbar_right_text02)).inflate();
             mTvRight02 = (TextView) findViewById(R.id.tv_comactionbar_right02);
+            mTvRight02.setTextColor(allTextColor);
+            mTvRight02.setTextSize(TypedValue.COMPLEX_UNIT_PX, itemTextSize);
         }
     }
 
@@ -385,7 +443,8 @@ public class ComActionBar extends FrameLayout
     public void setRightTextColor02(int color)
     {
         inflateRightTextView02();
-        mTvRight02.setTextColor(color);
+        if (color > 0)
+            mTvRight02.setTextColor(color);
     }
 
     //inflate the first ImageView in the right area
@@ -495,4 +554,33 @@ public class ComActionBar extends FrameLayout
     {
         mViewRight02.setOnClickListener(listener);
     }
+
+    public void setTitleTextSize(float size)
+    {
+        setTitleTextSize(TypedValue.COMPLEX_UNIT_SP, size);
+    }
+
+    public void setTitleTextSize(int unit, float size)
+    {
+        this.titleTextSize = TypedValue.applyDimension(unit, size, getResources().getDisplayMetrics());
+        if (mTvTitle != null)
+            mTvTitle.setTextSize(unit, size);
+    }
+
+    public void setItemTextSize(float size)
+    {
+        setItemTextSize(TypedValue.COMPLEX_UNIT_SP, size);
+    }
+
+    public void setItemTextSize(int unit, float size)
+    {
+        this.itemTextSize = TypedValue.applyDimension(unit, size, getResources().getDisplayMetrics());
+        if (mTvLeft != null)
+            mTvLeft.setTextSize(unit, size);
+        if (mTvRight01 != null)
+            mTvRight01.setTextSize(unit, size);
+        if (mTvRight02 != null)
+            mTvRight02.setTextSize(unit, size);
+    }
+
 }
--- README.md
+++ README.md
@@ -9,7 +9,7 @@ ComActionBar：通用的自定义ActionBar
 
 ```
 dependencies{
-         compile 'com.lwkandroid.widget:ComActionBar:1.0.2
+         compile 'com.lwkandroid.widget:ComActionBar:1.1.0
     }
 ```
 <br/>
@@ -23,7 +23,10 @@ dependencies{
         android:layout_marginTop="10dp"
         app:bg_color="@android:color/holo_orange_dark" //背景色，默认为colorPrimary
         app:is_left_as_back="true" //设置左侧功能为“返回”,默认为false
+        app:is_left_as_back_without_text="true" //设置左侧功能为“返回”，不带文案，默认为false
+        app:left_back_drawable="@drawable/ic_back" //设置“返回”箭头
         app:left_image="@drawable/cab_left" //设置左侧图片
+        app:all_text_color="@android:color/holo_red_light" //设置全局文字颜色，默认白色
         app:left_text="good" //设置左侧文案
         app:left_text_color="@android:color/holo_purple" //设置左侧文案文字颜色
         app:title_text="Title" //设置标题文案
@@ -34,6 +37,8 @@ dependencies{
         app:right_text02="HAHA" //设置右侧第二个文案
         app:right_text_color01="@android:color/holo_green_dark" //设置右侧第一个文案文字颜色
         app:right_text_color02="@android:color/holo_green_dark" //设置右侧第二个文案文字颜色
+        app:text_size_item="14sp" //设置除标题外文字大小
+        app:text_size_title="22sp" //设置标题文字大小
         />
 ```
 <br/>
