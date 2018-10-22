c188ffe90b2537f09b5e557b31c4ae445c0de462
luowenkang <505515031@qq.com>
2018-10-22 15:51:42 +0800

    demo 适配AndroidX

--- app/src/main/java/com/lwkandroid/demo/MainActivity.java
+++ app/src/main/java/com/lwkandroid/demo/MainActivity.java
@@ -1,11 +1,12 @@
 package com.lwkandroid.demo;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
 
 import com.lwkandroid.widget.comactionbar.ComActionBar;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity
 {
