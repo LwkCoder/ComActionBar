f45ffdf9f8750cb46f92b62b85b05efd7c835fe0
luowenkang <505515031@qq.com>
2021-07-12 10:57:45 +0800

    2.3.0

--- library/src/main/java/com/lwkandroid/widget/cab/ComActionBar.java
+++ library/src/main/java/com/lwkandroid/widget/cab/ComActionBar.java
@@ -13,7 +13,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import com.lwkandroid.widget.R;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
